c2c39e52104c6383c51e38f8e045e05439458ede
suhaspatil1703-ui <suhaspatil1703@gmail.com>
2025-11-21 18:52:40 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,7 +7,7 @@ log() { echo "$1"; }
 
 export AWS_REGION
 
-# TERMINATE EC2
+
 INSTANCE_IDS=$(aws ec2 describe-instances \
   --filters "Name=tag:Name,Values=$INSTANCE_NAME" \
   --query "Reservations[].Instances[].InstanceId" --output text)
@@ -20,7 +20,6 @@ else
   log "No EC2 instances found."
 fi
 
-# DELETE SECURITY GROUP
 if aws ec2 describe-security-groups --group-names "$SEC_GROUP_NAME" &>/dev/null; then
   log "Deleting security group $SEC_GROUP_NAME"
   SG_ID=$(aws ec2 describe-security-groups --group-names "$SEC_GROUP_NAME" --query 'SecurityGroups[0].GroupId' --output text)
@@ -29,7 +28,7 @@ else
   log "Security group not found."
 fi
 
-# DELETE KEY PAIR
+
 if aws ec2 describe-key-pairs --key-names "$KEY_PAIR_NAME" &>/dev/null; then
   aws ec2 delete-key-pair --key-name "$KEY_PAIR_NAME"
   log "Key pair deleted."
@@ -39,7 +38,7 @@ if [ -f "${KEY_PAIR_NAME}.pem" ]; then
   rm -f "${KEY_PAIR_NAME}.pem"
 fi
 
-# DELETE S3 BUCKETS
+
 log "Searching for buckets starting with prefix: $S3_BUCKET_PREFIX"
 
 BUCKETS=$(aws s3api list-buckets --query "Buckets[].Name" --output text)
